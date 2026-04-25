bbea4f391fcf44e0b15ad0aa7be115739362784b
jklia <prostotak1861@gmail.com>
2026-04-25 22:13:07 +0500

    Menu draft

---
+++ Assets/Scripts/MenuScripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MenuScripts
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public void PlayGame()
+        {
+            SceneManager.LoadScene("MainScene");
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+            // Debug.Log("Выход из игры"); // работает в редакторе
+        }
+    }
+}
