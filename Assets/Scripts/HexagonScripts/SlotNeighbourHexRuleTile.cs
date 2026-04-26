909e2e3f1b540d8726bd02716e9e8843f8eb2f71
jklia <prostotak1861@gmail.com>
2026-04-26 15:04:51 +0500

    Neighbours and slot tiles

---
+++ Assets/Scripts/HexagonScripts/SlotNeighbourHexRuleTile.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace HexagonScripts
+{
+    [CreateAssetMenu(fileName = "SlotNeighbourHexRuleTile",
+        menuName = "2D/Tiles/Slot Neighbour Hex Rule Tile")]
+    public class SlotNeighbourHexRuleTile : HexagonalRuleTile
+    {
+        [Tooltip("SlotTile")]
+        public TileBase slotTile;
+
+        public override bool RuleMatch(int neighbor, TileBase other)
+        {
+            switch (neighbor)
+            {
+                // 1 клик в редакторе правил = "This" => проверяем на slotTile
+                case TilingRuleOutput.Neighbor.This:
+                    return other != null && other == slotTile;
+
+                // 2 клика = "NotThis" => всё, что НЕ slotTile (включая пустоту)
+                case TilingRuleOutput.Neighbor.NotThis:
+                    return other != slotTile;
+            }
+            return base.RuleMatch(neighbor, other);
+        }
+    }
+}
