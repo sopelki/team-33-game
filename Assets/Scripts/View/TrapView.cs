6d441ce70571005083cecd64d0d20d535d5422f6
jklia <prostotak1861@gmail.com>
2026-05-19 16:16:37 +0500

    Trap XRay

--- Assets/Scripts/View/TrapView.cs
+++ Assets/Scripts/View/TrapView.cs
@@ -5,10 +5,48 @@ namespace View
     public class TrapView : MonoBehaviour
     {
         [SerializeField] private SpriteRenderer spriteRenderer;
-
+        [Header("X-Ray Settings")]
+        [SerializeField] private GameObject outline;
+        
+        private int occludersCount = 0;
         public void Initialize(Sprite sprite)
         {
             spriteRenderer.sprite = sprite;
+            SetOutlineVisible(false);
+            occludersCount = 0;
         }
+        
+        public void SetOutlineVisible(bool value)
+        {
+            if (outline != null)
+                outline.SetActive(value);
+        }
+        
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Tower"))
+            {
+                if (transform.position.y > other.transform.position.y)
+                {
+                    occludersCount++;
+                    SetOutlineVisible(occludersCount > 0);
+                }
+            }
+        }
+        
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.CompareTag("Tower"))
+            {
+                if (transform.position.y > other.transform.position.y)
+                {
+                    occludersCount--;
+                    if (occludersCount < 0) 
+                        occludersCount = 0; 
+                    SetOutlineVisible(occludersCount > 0);
+                }
+            }
+        }
+
     }
 }
